fe74c724e7c0fd9030548c70afe0a878dc864860
Andrés Bárcena <barcena890@gmail.com>
2025-07-02 15:39:44 -0500

    Primer commit - Proyecto gestion de usuarios

---
+++ src/test/java/com/andres/gestion_usuarios_api/GestionUsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.gestion_usuarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
